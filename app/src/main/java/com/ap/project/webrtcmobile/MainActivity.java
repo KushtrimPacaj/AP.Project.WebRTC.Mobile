93f3be0cd19c6831b11dad3c9bc31325c8314547
Kushtrim Pacaj <kushtrimpacaj@gmail.com>
2020-02-29 09:56:08 +0100

    ViewBinding! Yay Android Studio 3.6

--- app/src/main/java/com/ap/project/webrtcmobile/MainActivity.java
+++ app/src/main/java/com/ap/project/webrtcmobile/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.ap.project.webrtcmobile.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
